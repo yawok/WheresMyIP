85e18ee95c35514e9fd93cccc8c4e3cd7ba485b3
yaw.o.k <obengky72@gmail.com>
2023-09-17 23:59:28 +0200

    Fix styling

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from . import api
 def index(request):
     ip = api.get_ip()
     info = api.get_info(ip)
+    print(info)
     
     return render(request, "index.html", info)
     
--- wheresmyip/settings.py
+++ wheresmyip/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-64xj2uge-=cz&b1z1q0$9iuzc3ll8qnv-evv3kgul)crv4)n$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
